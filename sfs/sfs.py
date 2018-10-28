b32c7b16cf0f64005fa36d3a0103df125fa5ff9f
Devon Rudd <nukeguy5@MacBook-Pro-3.local>
2018-10-28 16:42:26 -0700

    Updated sfs project

---
+++ sfs/sfs.py
@@ -0,0 +1,50 @@
+
+from diskpy import Disk
+import click
+import numpy as np
+from threading import Lock
+
+disk1 = Disk('disk1', 16)
+fs_bitmap = np.zeros(shape=(disk1.disk_size(), 1), dtype='int32')
+file_table = {}
+fs_lock = Lock()
+
+def fs_format():
+    with fs_lock:
+        for i in range(len(fs_bitmap)):
+            fs_bitmap[i] = 0
+
+def fs_debug():
+    print(' ------ Disk Mgmt -------')
+    for i in range(len(fs_bitmap)):
+        print(i, ':', fs_bitmap[i])
+    print(' _______ End Mgmt _______')
+    print()
+    print(' --------- Disk ---------')
+    for i in range(len(disk1.disk)):
+        print(i, ':', disk1.disk[i])
+    print(' _______ End Disk _______')
+    print()
+
+def fs_mount():
+    pass
+
+def fs_create():
+    pass
+
+def fs_delete(file):
+    pass
+
+def fs_getsize(file):
+    pass
+
+def fs_read(file, length, offset):
+    with fs_lock:
+        pass
+
+def fs_write(file, data, length, offset):
+    with fs_lock:
+        fs_bitmap[offset] = 1
+
+
+fs_debug()
