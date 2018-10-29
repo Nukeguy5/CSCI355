a5591013137f2ed27fccbf2d232fc75551ff1a9a
Devon Rudd <nukeguy5@MacBook-Pro-3.local>
2018-10-28 23:32:54 -0700

    Updated disk.py to create binary file of "disk"

--- sfs/diskpy.py
+++ sfs/diskpy.py
@@ -12,10 +12,13 @@ class Disk():
         self.disk = np.zeros(shape=(nblocks, Disk.DISK_BLOCK_SIZE), dtype='int16')
         self.DISK_LOCK = Disk.DISK_LOCK
 
+        with self.DISK_LOCK:
+            with open(self.filename, 'wb') as f:
+                f.write(self.disk)
+
     def disk_open(self, filename):
         with self.DISK_LOCK:
-            with open(filename, 'wb') as f:
-                f.write(self.disk)
+            pass
 
     def disk_size(self):
         # print("Disk size:", self.nblocks*Disk.DISK_BLOCK_SIZE, "bytes")
@@ -42,4 +45,3 @@ class Inode():
     pass
 
 disk1 = Disk('disk1.bin', 16)
-disk1.disk_open('disk1.bin')
